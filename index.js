631b502116127119ec4f4527b9de916b4bcdf53f
batdev2 <125290013+batdev2@users.noreply.github.com>
2024-03-10 17:36:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("firstProject").onclick = function() {
+  location.href = "https://batdev2.github.io/tindog-website/";
+}
+
+document.getElementById("secondProject").onclick = function() {
+    location.href = "https://batdev2.github.io/move-it/";
+}
+
+document.getElementById("navProjects").onclick = function() {
+    location.href = "#projects";
+}
+
+document.getElementById("navContact").onclick = function() {
+    location.href = "#contact";
+}
